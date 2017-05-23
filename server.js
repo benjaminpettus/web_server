8bf16d1d2849eaf383a3e6f29370901f321890f0
benjaminpettus <benjaminpettus@gmail.com>
2017-05-22 18:10:14 -0700

    home route rendering, WIP styling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
 const PORT = 3000
 
 
@@ -9,11 +10,20 @@ const PORT = 3000
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname + '/views'))
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
+const artists = require("./data/artists.json")
+const albums = require("./data/albums.json")
+const songs = require("./data/songs.json")
+
 app.get('/', (req, res) => {
-  res.render('index', { pageTitle: "Music Stuffs" })
+
+  res.render('index', { pageTitle: "Music Stuffs",
+                        page: "Artists",
+                        data: JSON.stringify(artists)
+                      })
 })
 
 
